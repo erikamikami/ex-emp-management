42517480b63d3de26ac0235b3f25b10505ae358b
erikamikami <g.erika3up@gmail.com>
2022-05-13 14:33:21 +0900

    管理者ログイン機能の実装完了 動作確認済み

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,6 +30,9 @@ public class AdministratorController {
 	LoginForm setUpLoginForm() {
 		return new LoginForm();
 	}
+
+	@Autowired
+	private HttpSession session;
 
 	@RequestMapping("/toInsert")
 	public String toInsert() {
@@ -55,6 +61,30 @@ public class AdministratorController {
 	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login";
+	}
+
+	/**
+	 * ログイン判定を行う処理. serviceのloginメソッドの結果がnullの場合：ログインエラーメッセージを表示し、ログイン画面にフォワードする
+	 * serviceのloginメソッドの結果がnullでなかった場合：ログインエラーメッセージを表示し、従業員情報一覧ページにフォワードする
+	 * 
+	 * @param loginForm
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/login")
+	public String login(LoginForm loginForm, Model model) {
+		String mailAddress = loginForm.getMailAddress();
+		String password = loginForm.getPassword();
+		Administrator administrator = service.login(mailAddress, password);
+
+		if (administrator == null) {
+			model.addAttribute("loginErrorMessage", "メールアドレスまたはパスワードが不正です。");
+			return "forward:/";
+		} else {
+			String administratorName = administrator.getName();
+			session.setAttribute("administratorName", administratorName);
+			return "forward:/employee/showList";
+		}
 	}
 
 }
